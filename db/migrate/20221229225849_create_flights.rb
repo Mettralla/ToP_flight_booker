ea77fc0f0313dd7c1160b73cd5f3cb77d42509a0
Mettralla <daxter0493@gmail.com>
2022-12-30 01:41:00 -0300

    Flight model, Airport-Flight Associations and database seeds added

---
+++ db/migrate/20221229225849_create_flights.rb
@@ -0,0 +1,15 @@
+class CreateFlights < ActiveRecord::Migration[7.0]
+  def change
+    create_table :flights do |t|
+      t.datetime :date
+      t.integer :duration
+      t.integer :passagers
+      t.references :departure_airport, null: false
+      t.references :arrival_airport, null: false
+
+      t.timestamps
+    end
+    add_foreign_key :flights, :airports, column: :departure_airport_id
+    add_foreign_key :flights, :airports, column: :arrival_airport_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_29_033513) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_29_225849) do
   create_table "airports", force: :cascade do |t|
     t.string "name"
     t.string "city"
@@ -19,4 +19,18 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_29_033513) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "flights", force: :cascade do |t|
+    t.datetime "date"
+    t.integer "duration"
+    t.integer "passagers"
+    t.integer "departure_airport_id", null: false
+    t.integer "arrival_airport_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["arrival_airport_id"], name: "index_flights_on_arrival_airport_id"
+    t.index ["departure_airport_id"], name: "index_flights_on_departure_airport_id"
+  end
+
+  add_foreign_key "flights", "airports", column: "arrival_airport_id"
+  add_foreign_key "flights", "airports", column: "departure_airport_id"
 end
